c27977cdfe7149b97edfb456cd752cad967fa42d
zhongyehai <907728701@qq.com>
2024-03-29 09:48:00 +0800

    优化

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ from urllib import parse
 import urllib3.fields as f
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
-from apps.enums import WebHookTypeEnum
 from utils.client.test_runner import built_in as assert_func_file
 from utils.client.test_runner.webdriver_action import Actions
 
@@ -18,7 +17,7 @@ _job_server_port = 8025  # job服务端口
 _job_server_host = f'http://localhost:{_job_server_port}/api/job/status'  # job服务接口
 
 # 默认的webhook地址，用于接收系统状态通知、系统异常/错误通知...
-_default_web_hook_type = WebHookTypeEnum.ding_ding  # 默认通知的webhook类型，见枚举类
+_default_web_hook_type = 'ding_ding'  # 默认通知的webhook类型，见枚举类apps.enums.WebHookTypeEnum
 _default_web_hook = ''
 _web_hook_secret = ''  # secret，若是关键词模式，不用设置
 
