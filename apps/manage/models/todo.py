34f24c6d6e0d4bd78ac5e91f3ca31bb1fb5a2f86
zhongyehai <907728701@qq.com>
2024-04-30 15:04:20 +0800

    更新

--- apps/manage/models/todo.py
+++ apps/manage/models/todo.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import String
+from datetime import datetime
+
+from sqlalchemy import String, DateTime, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from apps.base_model import NumFiled
@@ -12,5 +14,7 @@ class Todo(NumFiled):
     __table_args__ = {"comment": "待处理任务"}
     status: Mapped[TodoListEnum] = mapped_column(String(128), default=TodoListEnum.todo, comment="状态")
     title: Mapped[str] = mapped_column(String(512), nullable=True, default="", comment="任务title")
+    done_user: Mapped[int] = mapped_column(Integer(), nullable=True, comment="完成人")
+    done_time: Mapped[datetime] = mapped_column(DateTime, nullable=True, comment="完成时间")
     # tags: Mapped[str] = mapped_column(String(512), nullable=True, default="", comment="tag")
     detail: Mapped[str] = mapped_column(String(2048), nullable=True, default="", comment="任务详情")
--- apps/manage/views/todo.py
+++ apps/manage/views/todo.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
-from flask import current_app as app
+from datetime import datetime
+
+from flask import current_app as app, g
 
 from ..blueprint import manage
 from ..forms.todo import GetTodoForm, DeleteTodoForm, AddTodoForm, ChangeTodoForm, ChangeStatusForm
 from ..model_factory import Todo
 from ...base_form import ChangeSortForm
+from ...enums import TodoListEnum
 
 
 @manage.login_get("/todo/list")
 def manage_get_todo_list():
     query_list = Todo.db.session.query(
-        Todo.id, Todo.title, Todo.status, Todo.create_user, Todo.create_time).order_by(Todo.num.asc()).all()
-    title_list = ["id", "title", "status", "create_user", "create_time"]
+        Todo.id, Todo.title, Todo.status, Todo.create_user, Todo.create_time, Todo.done_user, Todo.done_time
+    ).order_by(Todo.num.asc()).all()
+    title_list = ["id", "title", "status", "create_user", "create_time", "done_user", "done_time"]
     data_list = [dict(zip(title_list, data)) for data in query_list]
     return app.restful.get_success(data_list)
 
@@ -27,7 +31,10 @@ def manage_change_todo_sort():
 @manage.login_put("/todo/status")
 def manage_change_todo_status():
     form = ChangeStatusForm()
-    form.todo.model_update({"status": form.status})
+    done_user = done_time = None
+    if form.status == TodoListEnum.done:  # 改为完成
+        done_user, done_time = g.user_id, datetime.now()
+    form.todo.model_update({"status": form.status, "done_user": done_user, "done_time": done_time})
     return app.restful.change_success()
 
 
